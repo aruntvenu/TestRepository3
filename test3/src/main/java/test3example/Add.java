9c505c76326362b3892ffa612e7cb376aaed1737
asus <asus@user>
2023-10-17 20:18:53 +0530

    rep3

---
+++ test3/src/main/java/test3example/Add.java
@@ -0,0 +1,18 @@
+package test3example;
+
+public class Add {
+	
+	public void print()
+	{
+		System.out.println("Hello world");
+	}
+
+	public static void main(String[] args) 
+	{
+		
+		
+Add ch=new Add();
+ch.print();
+	}
+
+}
